1d30af055625ddbd609915c79ee810f2f1552f4d
mxnty <66350968+mxnty999@users.noreply.github.com>
2020-12-22 00:41:51 -0600

    mxnty

--- Firo bot/DJS-Template-main/src/commands/info/mxnty.js
+++ Firo bot/DJS-Template-main/src/commands/info/mxnty.js
@@ -10,10 +10,10 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(embed);
 }
 module.exports.config = {
-    name: 'ping',
-    description: 'checks the latency of the discord bots connection to discords API',
-    category: 'info',
-    usage: 'ping',
+    name: 'mxnty',
+    description: 'mxnty cmd :)',
+    category: 'fun',
+    usage: 'mxnty',
     example: '...',
   
     ownerOnly: true,
