d2a48ebc7c7431d7d5d9c43ad8132c206cc5e14c
Azlxy <62254752+Azlxy@users.noreply.github.com>
2020-12-26 13:53:19 +0000

    Update kick.js

--- Firo bot/DJS-Template-main/src/commands/moderation/kick.js
+++ Firo bot/DJS-Template-main/src/commands/moderation/kick.js
@@ -8,21 +8,27 @@ module.exports.run = async (client, message, args) => {
             || message.guild.members.cache.find(mem => mem.user.tag.toLowerCase() === args.slice(0).join(' ').toLowerCase());
     
     if (!member) return message.channe.send('Please provide a member to kick.');
+    if (member.id === message.author.id) return message.channel.send('You can\'t kick yourself.');
+    if (member.roles.highest.position >= message.member.roles.highest.position && message.guild.owner.id !== message.author.id) return message.channel.send('You can\'t kick someone that has a higher role or the same position than you.');
+    if (member.roles.highest.position >= message.guild.me.roles.highest.position && message.guild.owner.id !== client.user.id) return message.channel.send('I can\'t kick someone that has a higher role or the same position than me.');
     
     let reason = args.slice(1).join(' ');
+    if (!reason) reason = 'No reason given';
     
     const embed = new MessageEmbed()
     .setColor('ORANGE')
     .setAuthor('Kick', message.author.displayAvatarURL({dynamic:true}))
     .addField('Member', `${member.user.tag} (${member.id})`, true)
     .addField('Moderator', `${message.author.tag} (${message.author.id})`, true)
+    .setFooter('Ethereal Ark Bot by xFijo')
     .setTimestamp()
-    if (reason) embed.addField('Reason', reason, true);
+    embed.addField('Reason', reason, true);
     
     let c = message.guild.channels.cache.find(c => c.name.includes('log'));
 
     await member.kick({reason: reason});
     message.channel.send(embed).then(m => m.delete({timeout: 10000}));
+    member.send(`You got kicked from ${message.guild.name} for ${reason}`);
     
     if (c) c.send(embed);
 }
