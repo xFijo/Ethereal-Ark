826f3451686da3242db168e7001a12c775003ba7
xFijo <75216577+xFijo@users.noreply.github.com>
2020-12-26 12:07:45 +0000

    Update serverinfo.js

--- Firo bot/DJS-Template-main/src/commands/info/serverinfo.js
+++ Firo bot/DJS-Template-main/src/commands/info/serverinfo.js
@@ -5,7 +5,7 @@ const no = '<:no:782667011665952799>';
 
 module.exports.run = async (client, message, args) => {
   const embed = new MessageEmbed()
-  .setColor(10181046)
+  .setColor(1752220)
   .setThumbnail(message.guild.iconURL({dynamic:true}))
   .setAuthor(message.guild.name, message.guild.iconURL({dynamic:true}))
   .addField('General', `Owner: ${message.guild.owner}\nRegion: ${message.guild.region}\nMembers: ${message.guild.memberCount}\nText Channels: ${message.guild.channels.cache.filter(x => x.type === 'text').size}\nVoice Channels: ${message.guild.channels.cache.filter(x => x.type === 'voice').size}`)
