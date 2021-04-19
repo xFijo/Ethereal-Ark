673ca1b87eb915add4b6072474fa61c8c33f6be5
xFijo <75216577+xFijo@users.noreply.github.com>
2021-04-19 22:08:48 +0100

    Update config.js

--- Firo bot/DJS-Template-main/config.js
+++ Firo bot/DJS-Template-main/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: 'NzkwNzAwMTk5OTI4NTI4ODk3.X-Ea8g.O3rH5l4jertVIroJWbW44F_YT1M', // Bot token.
-    prefix: '$', // Bot prefix.
-    owners: ['612670639551021059 , 364214160290217994'], // An array of user ids who are counted as bot owner.
+    token: '', // Bot token.
+    prefix: '', // Bot prefix.
+    owners: [], // An array of user ids who are counted as bot owner.
 
     commandCooldown: 0 // Must be an integer (number), 0 if you don't want a cooldown.. 
 }
